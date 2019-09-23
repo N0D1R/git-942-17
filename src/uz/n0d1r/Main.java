a57e57bb1a9ae1b9e0d15fcdb65b3ada1464624c
N0D1R <n.sadullayev@gmail.com>
2019-09-23 17:15:04 +0500

    test funksiya yozildi

--- src/uz/n0d1r/Main.java
+++ src/uz/n0d1r/Main.java
@@ -4,8 +4,33 @@ public class Main {
 
     public static void main(String[] args) {
 
-        testFunction();
+        int a[] = new int [100];
+        int x = 5;
 
+        testFunction();
+        sorting(a);
+        int index = binSearch(a, x);
+
+    }
+
+    // TODO: 23-Sep-19 Mardonbek
+    /*
+    Berilgan a massivni o'sish tartibida saralaydigan funksiya
+     */
+    private static void sorting(int[] a) {
+
+    }
+
+    // TODO: 23-Sep-19 Binary Search algoritmi yoziladi. Shohruh
+    /*
+    A massiv saralangan tartibda beriladi.
+    Ushbu massivdan x soni qidirilsin
+    agar x mavjud bo'lsa birinchi indeksi, aks xolatda
+    -1 qiymat qaytarilsin
+     */
+    private static int binSearch(int[] a, int x) {
+
+        return 0;
     }
 
     private static void testFunction() {
