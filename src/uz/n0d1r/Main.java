d9189ec08a189490ac8d5d0f896988f4c9ea753d
N0D1R <n.sadullayev@gmail.com>
2019-09-23 17:05:53 +0500

    test funksiya yozildi

--- src/uz/n0d1r/Main.java
+++ src/uz/n0d1r/Main.java
@@ -3,6 +3,12 @@ package uz.n0d1r;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        testFunction();
+
+    }
+
+    private static void testFunction() {
+
     }
 }
